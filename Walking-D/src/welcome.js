5571b5fd6412e0279c4c5cece23406a7c0277f47
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2023-05-29 04:13:54 +0530

    Update welcome.js

--- Walking-D/src/welcome.js
+++ Walking-D/src/welcome.js
@@ -1,10 +1,17 @@
-import React from "react";
+import {useState,useEffect} from "react";
 
 
 function Welcome(){
+    const [User,setUser] = useState('')
+   
+    useEffect(() => {
+        const GetUser = localStorage.getItem('enteredEmail');
+        setUser(GetUser);
+      }, []);
     return(
-        <div>
+        <div id="email">
             <h1>Welcome</h1>
+            <span>User :- {User}</span>
         </div>
     )
 
