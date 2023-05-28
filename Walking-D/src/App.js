dcc77a9e1796f473ea417cc11f15ab64ca4d3957
Shubh007ox <110490602+Shubh007ox@users.noreply.github.com>
2023-05-29 04:01:21 +0530

    Update App.js

--- Walking-D/src/App.js
+++ Walking-D/src/App.js
@@ -1,22 +1,22 @@
 import React, { useContext } from "react";
-import LoginPage from "./Components/Client/Login";
 import { Navigate, Route, Routes } from "react-router-dom";
 import AuthForm from "./Components/Client/authForm";
-import FacebookPage from "./Components/Client/facebookPage";
-import AuthContext from "./Components/Server/auth-context"; // Corrected import
-import Welcome from "./welcome";
+import AuthContext from "./Components/Server/auth-context";
+import Welcome from "./Components/Client/welcome";
+import MainNavigation from "./Components/Client/navbar";
+
 
 function App() {
   const authCtx = useContext(AuthContext);
 
   return (
     <React.Fragment>
+        <MainNavigation />
       <Routes>
-        <Route path="/" element={<LoginPage />} />
-        <Route path="/authForm" element={<AuthForm />} />
-        <Route path="/facebook" element={<FacebookPage />} />
+        <Route path="/" element={<AuthForm />} />
         <Route path="/welcome" element={authCtx.isLoggedIn ? <Welcome /> : <Navigate to="/" />} />
       </Routes>
+
     </React.Fragment>
   );
 }
